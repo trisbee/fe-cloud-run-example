e5e460437c11c9b28b634353ce485b7a2e27698a
Michal Landsman <landsman@insuit.cz>
2020-08-20 16:56:35 +0200

    try env vars

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const packageData = require('../package');
 
 // settings
+const NODE_ENV = process.env.NODE_ENV;
 const PORT = process.env.APP_PORT || 3000;
 const HOST = 'localhost';
 
@@ -11,8 +12,8 @@ const HOST = 'localhost';
 const commitHash = process.env.COMMIT_HASH || 'not set';
 const GC_revision = process.env.K_REVISION || 'not set';
 const GC_service = process.env.K_SERVICE || 'not set';
-const GC_project = process.env.GCP_PROJECT || 'not set';
-const GC_region = process.env.FUNCTION_REGION || 'not set';
+const GC_project = process.env.K_PROJECT || 'not set';
+const GC_region = process.env.K_REGION || 'not set';
 const cloudURL = `https://console.cloud.google.com/run/detail/${GC_region}/${GC_service}/revisions&project=${GC_project}`;
 
 // app
@@ -23,9 +24,10 @@ app.get('/', (req, res) => {
     <h1>Hello World</h1>
     <h2>data:</h2>
     <ul>
-        <li>package version: <strong>${packageData.version}</strong></li>
-        <li>commit hash: <strong>${commitHash}</strong></li>
+        <li>Package version: <strong>${packageData.version}</strong></li>
+        <li>Commit hash: <strong>${commitHash}</strong></li>
         <li>Google Cloud revision id: <a href="${cloudURL}" target="_blank"><strong>${GC_revision}</strong></a></li>
+        <li>Node ENV: <strong>${NODE_ENV}</strong></li>
     </ul>
     `;
 
